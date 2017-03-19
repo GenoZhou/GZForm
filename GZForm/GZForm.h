e3d40300308d6fe36fbf78796c57cfb84ae1cc1f
Hanxuan <genozhou@GenoZhou-Macbook-Pro.local>
2017-03-18 20:36:35 -0400

    Init project;

---
+++ GZForm/GZForm.h
@@ -0,0 +1,19 @@
+//
+//  GZForm.h
+//  GZForm
+//
+//  Created by GenoZhou on 2017-03-18.
+//  Copyright © 2017 GenoZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GZForm.
+FOUNDATION_EXPORT double GZFormVersionNumber;
+
+//! Project version string for GZForm.
+FOUNDATION_EXPORT const unsigned char GZFormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GZForm/PublicHeader.h>
+
+
